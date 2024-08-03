17d42a6eb9fe0aec51dd67b16ffc8014a89f5c8e
ROHAN-WAGH-ROOT <rohanwagh234@gmail.com>
2024-08-03 13:08:22 +0530

    first comment

---
+++ src/App.tsx
@@ -0,0 +1,35 @@
+import React from 'react';
+import './App.css';
+import {
+  createBrowserRouter,
+  RouterProvider,
+  Route,
+  Link,
+} from "react-router-dom";
+
+
+function App() {
+  const router = createBrowserRouter([
+    {
+      path: "/",
+      element: (
+        <div>
+          <h1>Hello World</h1>
+          <Link to="about">About Us</Link>
+        </div>
+      ),
+    },
+    {
+      path: "about",
+      element: <div>About</div>,
+    },
+  ]);
+  
+  return (
+    <div className="text-6xl">
+        <RouterProvider router={router} />
+    </div>
+  );
+}
+
+export default App;
